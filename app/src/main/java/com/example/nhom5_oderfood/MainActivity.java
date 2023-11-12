67eae626a28873330a224d07783fd85dfb1b85ce
admin <cankhiemzx@gmail.com>
2023-11-12 21:28:39 +0700

    khiem cmt

--- app/src/main/java/com/example/nhom5_oderfood/MainActivity.java
+++ app/src/main/java/com/example/nhom5_oderfood/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //quang comment
         //quang comment2
+        //khiem comment
     }
 }
