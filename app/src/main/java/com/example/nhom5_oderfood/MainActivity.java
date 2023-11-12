f90de1d623cea72a35c8f5e4af05af6d332ee550
admin <cankhiemzx@gmail.com>
2023-11-12 22:16:11 +0700

    khiem cmt

--- app/src/main/java/com/example/nhom5_oderfood/MainActivity.java
+++ app/src/main/java/com/example/nhom5_oderfood/MainActivity.java
@@ -10,10 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //quang comment
-        //quang comment2
-        //khiem comment
-        //Thicoment
-        //khiem comment2
+
     }
 }
