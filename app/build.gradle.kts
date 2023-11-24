e09426ed1d0f1cae5ec25887b6ea0d38908c27a0
Thinguyen04 <inuyasha4002@gmail.com>
2023-11-24 20:34:11 +0700

    thêm, sửa, xóa, xem món ăn màn admin còn ảnh đang làm

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,5 +55,5 @@ dependencies {
 
     implementation ("androidx.room:room-runtime:2.6.0")
     annotationProcessor ("androidx.room:room-compiler:2.6.0")
-
+    implementation ("com.squareup.picasso:picasso:2.8")
 }
